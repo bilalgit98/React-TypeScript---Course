69bd1a0fe91ec2c755b39e9584fb1d591a2cae24
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-04 23:17:32 +0000

    Setup React and TypeScript Project and Basic Types in Typescript

---
+++ typescript-course/src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+let name: string = "bilal";
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+// let name: string;
+// name = 7; // not possible because the type is set to string
+// role: [5, 8]; //not possible because the type is set to number and string
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        <h1> HELLO WORLD</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
