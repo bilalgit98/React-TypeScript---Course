907204aa9a001d53d25028d13e8e79d7dfccea20
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-06 00:14:15 +0000

    Todo Complete & Delete Functionality

--- typescript-course/src/components/singleTodo.tsx
+++ typescript-course/src/components/singleTodo.tsx
@@ -9,18 +9,41 @@ type Props = {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
+
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((x) =>
+        x.id === id
+          ? {
+              ...todo,
+              isDone: !todo.isDone,
+            }
+          : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <form className="todos__single">
-      <span className="todos__single--text">{todo.todo}</span>
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
+
       <div>
         <span className="icon">
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
